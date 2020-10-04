224b876b542ab83b4531cd000283ae61c8f6e8a1
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2020-10-04 23:48:31 +0900

    change header style

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,19 @@
   <div class="p-body">
     <header class="l-header">
       <div class="p-header">
-        <div class="l-container">
-          <h1 class="p-header__title p-blogTitle u-flexCentering">
+        <div class="l-container u-flexCentering -tb">
+          <h1 class="p-header__title">
             <!-- <span class="u-screenReaderText" lang="en">Blog title</span> -->
-            <a href="./">
-              <p class="p-blogTitle__text" lang="en">NKTech</p>
+            <span class="u-screenReaderText" lang="en">NKTech</span>
+            <a href="./" class="c-logo c-logo--primary">
+              <img
+                src="../assets/images/common/logo-primary.png"
+                srcset="
+                  ../assets/images/common/logo-primary.png    1x,
+                  ../assets/images/common/logo-primary@2x.png 2x
+                "
+                alt="NKTech"
+              />
             </a>
           </h1>
         </div>
@@ -19,7 +27,7 @@
     </main>
     <footer class="l-footer">
       <div class="p-footer">
-        <article class="p-footer__copyright c-copyright u-flexCentering">
+        <article class="p-footer__copyright c-copyright u-flexCentering -fd">
           <small lang="en">&copy; 2020 Hikaru Nakata.</small>
         </article>
       </div>
