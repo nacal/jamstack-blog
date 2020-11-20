df5b1a89cf1c20fe703ca87b7da669e99c9d91bd
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2020-11-20 14:20:57 +0900

    fix stylelint setting

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,8 +3,10 @@ module.exports = {
     "stylelint-config-standard",
     "stylelint-config-recess-order",
     "stylelint-config-recommended-scss",
-    "stylelint-prettier/recommended",
+    //"stylelint-prettier/recommended",
   ],
   plugins: [],
-  rules: {},
+  rules: {
+    "number-leading-zero": "never",
+  },
 };
