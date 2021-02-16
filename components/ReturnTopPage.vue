ad184fe8df46f426ddc4aa86ce87897a9a658eea
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-02-16 22:02:58 +0900

    Develop ReturnTopPage component

---
+++ components/ReturnTopPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <article class="returnTopPage">
+    <nuxt-link
+      to="/"
+      class="_link">トップページへ</nuxt-link>
+  </article>
+</template>
+
+<style lang="scss" scoped>
+.returnTopPage {
+  margin-top: 72px;
+  text-align: center;
+
+  & ._link {
+    position: relative;
+    display: inline-block;
+    transition: .3s;
+
+    &::after {
+      position: absolute;
+      bottom: -2px;
+      left: 50%;
+      width: 0;
+      height: 2px;
+      content: '';
+      background-color: $main-color;
+      transition: .3s;
+      transform: translateX(-50%);
+    }
+
+    &:hover::after {
+      width: 100%;
+    }
+  }
+}
+</style>
