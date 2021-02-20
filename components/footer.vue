890380f1d62675c0fcc1418d778f8b9e1c99975f
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-02-20 22:20:03 +0900

    Fix global scss to scoped

--- components/footer.vue
+++ components/footer.vue
@@ -1,9 +1,23 @@
 <template>
   <footer>
-    <div class="p-footer">
-      <article class="p-footer__copyright c-copyright c-flex -ai-fe -jc-fe">
+    <div class="footer">
+      <article class="footer__copyright c-flex -ai-fe -jc-fe">
         <small lang="en">&copy; 2020 Hikaru Nakata.</small>
       </article>
     </div>
   </footer>
 </template>
+
+<style lang="scss" scoped>
+.footer {
+  height: 100%;
+  clip-path: polygon(0 calc(100vw / 5.37), 100% 0, 100% 101%, 0 101%);
+  background-color: $main-color;
+
+  &__copyright {
+    height: 100%;
+    padding: .5rem 1rem;
+    font-size: .75rem;
+  }
+}
+</style>
