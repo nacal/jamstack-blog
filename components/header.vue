b3a19ea93e5fb04c30f0e4eefbddb01c2e74e1a4
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-03-19 14:41:03 +0900

    Develop Links component

--- components/header.vue
+++ components/header.vue
@@ -2,42 +2,23 @@
   <header>
     <div class="header">
       <div class="header__container u-flexCentering -fd -fxd-c">
-        <div class="u-flexCentering -lr -fxd-c">
-          <h1 class="header__title">
-            <nuxt-link
-              to="/"
-              class="c-logo c-logo--primary">
-              <img
-                src="../assets/images/common/logo-primary.png"
-                srcset="
+        <h1 class="header__title">
+          <nuxt-link
+            to="/"
+            class="c-logo c-logo--primary">
+            <img
+              src="../assets/images/common/logo-primary.png"
+              srcset="
                     ../assets/images/common/logo-primary.png    1x,
                     ../assets/images/common/logo-primary@2x.png 2x
                   "
-                alt="NKTech"
-              >
-            </nuxt-link>
-          </h1>
-          <h2 class="header__copy">アウトプットをコツコツ積み上げるTechブログ</h2>
-        </div>
+              alt="NKTech"
+            >
+          </nuxt-link>
+        </h1>
+        <h2 class="header__copy">アウトプットをコツコツ積み上げるTechブログ</h2>
       </div>
-      <!-- /.l-container -->
-      <div class="header__sns p-sns c-flex">
-        <Icon
-          v-for="(icon, index) in icons"
-          :key="index"
-          :fa="icon.fa"
-          :class="'-main'"
-          :link="icon.link"
-          :aria-label="icon.label" />
-        <nuxt-link
-          to="/contact"
-        >
-          <fa
-            :icon="faEnvelope"
-            aria-label="お問い合わせのアイコン"
-            class="p-sns__item -main"/>
-        </nuxt-link>
-      </div>
+      <Links class="header__sns" />
       <div
         :class="{'is-active': active}"
         class="SearchForm">
@@ -52,45 +33,23 @@
           class="SearchForm__form"/>
       </div>
     </div>
-    <!-- /.p-header -->
+    <!-- /.header -->
   </header>
 </template>
 
 <script>
-import { faTwitter, faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faSearch, faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default {
   data: function() {
     return {
       active: false,
       navi: false,
-      icons: [
-        {
-          fa: faTwitter,
-          link: "https://twitter.com/nkthkr_",
-          label: "Twitterのアイコン"
-        },
-        {
-          fa: faGithub,
-          link: "https://github.com/nkthkr",
-          label: "GitHubのアイコン"
-        },
-      ]
     };
   },
   computed: {
-    faTwitter() {
-      return faTwitter;
-    },
-    faGithub() {
-      return faGithub;
-    },
     faSearch() {
       return faSearch;
-    },
-    faEnvelope() {
-      return faEnvelope;
     },
   },
   methods: {
