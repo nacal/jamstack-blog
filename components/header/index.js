c437a81f684dcc3ac037567d579cbe09f45ea005
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-04-10 09:43:38 +0900

    Clean code

--- components/header/index.js
+++ components/header/index.js
@@ -1,7 +1,7 @@
-import Header from './Header';
-import Links from './Links';
-import Search from './Search';
-import SiteTitle from './SiteTitle';
+import Header from './Header'
+import Links from './Links'
+import Search from './Search'
+import SiteTitle from './SiteTitle'
 
 export default Header
 export {Links, Search, SiteTitle}
