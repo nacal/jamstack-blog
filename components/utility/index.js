0ccd63552cf158730b4257c267cd56e2f934760b
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-04-10 09:43:32 +0900

    Develop utillity components entrypoint

---
+++ components/utility/index.js
@@ -0,0 +1,6 @@
+import Icon from './Icon'
+import Loading from './Loading'
+import ReturnTop from './ReturnTop'
+import ReturnTopPage from './ReturnTopPage'
+
+export {Icon, Loading, ReturnTop, ReturnTopPage}
